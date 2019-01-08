d79845d115158ee0f61355d8c9d4a8b9a0ffa77a
陈杰 <913846183@qq.com>
2019-01-08 17:55:52 +0800

    init

---
+++ CJFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJFM
+//
+//  Created by 陈杰 on 2019/1/8.
+//  Copyright © 2019 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJFM/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CJFM
+//
+//  Created by 陈杰 on 2019/1/8.
+//  Copyright © 2019 Eric. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
